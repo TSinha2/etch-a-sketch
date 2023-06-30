8b138d7544dab43b11b09ae49a506e6054c95fcd
Tanay Sinha <tanay.sinha@mail.utoronto.ca>
2023-06-30 18:44:16 +0530

    script.js: Add function to remove all grid items and resize grid based on slider

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function populate_grid(grid_size)
     }
 }
 
-populate_grid(100)
+populate_grid(100*100)
 
 function reset()
 {
@@ -22,4 +22,29 @@ function reset()
     {
         grid_items[i].style.backgroundColor = "#798b9c"
     }
+}
+
+function update()
+{
+    // Remove all grid-items
+    let grid_container = document.getElementsByClassName("grid-container") 
+    grid_container[0].innerHTML = ''
+
+    // Width | Height
+    let sliderValue = Number(document.getElementsByTagName("input")[0].value)
+    let squareWidth = Math.sqrt ( (500*500) / (sliderValue * sliderValue) )
+
+    // Generate new grid
+    const grid = document.querySelector(".grid-container");
+    for (let i = 0; i < sliderValue*sliderValue; i++)
+    {
+        let square = document.createElement('div'); 
+        square.classList.add('grid-item');
+        square.classList.add(i);
+        square.style.width=`${squareWidth}px`
+        square.style.height=`${squareWidth}px`
+        square.addEventListener("mouseover", function (){square.style.backgroundColor="transparent"});
+        grid.appendChild(square);
+    }
+
 }
